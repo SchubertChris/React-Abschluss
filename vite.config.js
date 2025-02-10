8385a88c96f2048828ffd1b766f4515e169653a6
SchubertChris <schubert_chris@ocketmail.com>
2025-02-10 16:20:02 +0100

    Check

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Falls du in einem Unterordner hosten willst, setze das hier
+  base: '/React-Abschluss/'  // 👈 Wichtig für GitHub Pages
 });
