0eef5b63ffa965f04766bcb998a76f22d33310e9
SchubertChris <schubert_chris@ocketmail.com>
2025-02-12 22:50:22 +0100

    edit Deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // Base-URL für GitHub Pages setzen
 export default defineConfig({
   plugins: [react()],
-  base: "/React-Abschluss/", // Stelle sicher, dass dies mit deinem Repository-Namen übereinstimmt!
+  base: "/React-Abschluss/", // Name deines Repos!
 });
