0d8070c66aff5f5b118b4fc3ace6d7b643281ee7
SchubertChris <schubert_chris@ocketmail.com>
2025-02-13 09:47:36 +0100

    404 Page angebunden und bearbeitet - Mond und Gif eingefügt

--- src/assets/pages/JSX/NotFound404.jsx
+++ src/assets/pages/JSX/NotFound404.jsx
@@ -14,7 +14,9 @@ export default function NotFound404() {
   return (
     <div className="not-found">
       <h1>404</h1>
-      <h2>Page Not Found</h2>
+      <h2>Nope, das war nichts</h2>
+      <div className='Mond'></div>
+      <img src="https://media.tenor.com/6JptszQgCnkAAAAi/text-work.gif" alt="Bild eines Pinguins, beim warten, dass was passiert." />
       <p>Sag mal, wo willst du denn hin? Komm ja zurück</p>
       <button className='backtoHome'>@Home</button>
     </div>
